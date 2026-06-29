eb99dd61ba12fe889af6cb5c7627d64440686b6a
AtharvaSontakke <atharvasotakke123@gmail.com>
2026-06-29 23:23:12 +0530

    Initial Spring Boot REST API

---
+++ springboot-devsecops/src/main/java/com/devsecops/springboot_devsecops/SpringbootDevsecopsApplication.java
@@ -0,0 +1,13 @@
+package com.devsecops.springboot_devsecops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevsecopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDevsecopsApplication.class, args);
+	}
+
+}
---
+++ springboot-devsecops/src/main/java/com/devsecops/springboot_devsecops/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.devsecops.springboot_devsecops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to DevSecOps CI/CD Pipeline";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is Healthy";
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "Version 1.0";
+    }
+}
